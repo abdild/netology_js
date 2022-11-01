d64546b1d3434822c4d97dd045ff38fc29e2481a
Абдуллин Ильдар Ильдусович <abdullinII@hq.tatneft.ru>
2022-11-01 09:36:30 +0300

    task_4

--- task_4/solution.js
+++ task_4/solution.js
@@ -6,7 +6,10 @@ function parseProducts(json) {
     // Преобразуйте строку json, переданную как аргумент функции,
     // в объект с помощью функции JSON.parse(json)
     // и запишите в переменную data
+    let data = JSON.parse(json);
     // Верните как результат функции свойство products объекта data
+    let products = data.products;
+    return products;
 }
 
 // Напишите функцию renderProductsCards(json)
@@ -16,3 +19,13 @@ function parseProducts(json) {
 // Запишите в переменную length значение свойства products.length
 // Напишите цикл, в котором перебираете все products от 0 до (length - 1)
 // и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
+function renderProductsCards(json){
+    // дальнейшие действия требуется выполнять тут
+    clearProducts();
+    let products = parseProducts(json);
+    let length = products.length;
+    for (let i = 0; i < length; i += 1) {
+        // место для пункта 7
+        addProduct(products[i]);
+     }
+ }
