cb183e676140eb887c324ad201d65cd729996a96
Абдуллин Ильдар Ильдусович <abdullinII@hq.tatneft.ru>
2022-11-03 14:00:27 +0300

    task final

--- task_final/solution.js
+++ task_final/solution.js
@@ -38,16 +38,33 @@ function sendRequest(name, phone, address, goods, sum) {
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
+    data.client = `${name} ${phone}`;
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
+    data.order.sum = `${sum}`;
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push({title: goods[i].title, count: goods[i].count});
+    }         
 
     let jsonData = JSON.stringify(data);
+    // console.log(jsonData);
 
     return jsonData;
+
+    // let data = {goods: [], order: {}};
+
+    // let countOfGoods = goods.length;
+
+    // for (let i = 0; i <= countOfGoods; i += 1) {
+    //     data.goods.push(goods[i].title);
+    // }
+
+    // data.order.address = address;
+    // data.order.sum = name + phone + address + goods + sum;
+
+    // data.client = 'Иван';
+
+    // let jsonData = JSON.stringify(data);
+
+    // return jsonData;
 }
